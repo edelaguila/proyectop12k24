ea964842a257ab960d80245d0abbca6134159a8d
BDG.Josue.Villagran <jvillagran@bdgsa.net>
2024-04-06 12:16:59 -0600

    Agregando atributos privados y funcion Set

--- include/Alumno.h
+++ include/Alumno.h
@@ -1,6 +1,7 @@
 #ifndef ALUMNO_H
 #define ALUMNO_H
 
+using namespace std;
 
 class Alumno
 {
@@ -8,9 +9,18 @@ class Alumno
         Alumno();
         virtual ~Alumno();
 
+        SetAlumno(string nombre,int edad)
+        {
+            this->edad = edad;
+            this->nombre = nombre;
+        }
+
     protected:
 
     private:
+        string nombre ;
+        int edad ;
+
 };
 
 #endif // ALUMNO_H
