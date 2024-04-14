7431be23f41454d76105b71151909df1a09d2940
Gabriela Pinto <156928142+gabspg@users.noreply.github.com>
2024-04-14 12:28:51 -0600

    Estudiante.h

--- SistemaUMG/include/Estudiante.h
+++ SistemaUMG/include/Estudiante.h
@@ -1,16 +1,19 @@
 #ifndef ESTUDIANTE_H
 #define ESTUDIANTE_H
 
+#include <iostream>
+#include <vector>
+#include <string>
 
-class Estudiante
-{
-    public:
-        Estudiante();
-        virtual ~Estudiante();
-
-    protected:
-
-    private:
+struct Estudiante {
+    std::string nombre;
+    int edad;
+    double promedio;
 };
+
+void mostrarEstudiantes(const std::vector<Estudiante>& estudiantes);
+void ingresarEstudiante(std::vector<Estudiante>& estudiantes);
+void modificarEstudiante(std::vector<Estudiante>& estudiantes);
+void eliminarEstudiante(std::vector<Estudiante>& estudiantes);
 
 #endif // ESTUDIANTE_H
