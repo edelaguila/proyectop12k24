badd4f55d5d5869bb1b66c2c748df099d15537b9
Oscar Morales <omoralesl7@miumg.edu.gt>
2024-05-03 17:51:29 -0600

    Menu General

--- Menu_Registros_Grupo1/main.cpp
+++ Menu_Registros_Grupo1/main.cpp
@@ -4,33 +4,38 @@ using namespace std;
 
 int main()
 {
-    int elec;
+    int opcion;
+    char continuar;
+    do {
+        system("cls");
 
-    cout<<"+-----------------------------------------------------------+"<< endl;
-    cout<<"+             Registro de Alumnos y Maestros                +"<< endl;
-    cout<<"+-----------------------------------------------------------+"<< endl;
-    cout<<"+   Indique si quiere agregar a un alumno o a un maestro    +" << endl;
-    cout<<"+-----------------------------------------------------------+"<< endl;
-    do
-    {
-    cout <<"       ->Ingrese su respuesta (alumno=1|maestros=2): ";
-    cin >> elec;
-    cout<<"+-----------------------------------------------------------+"<< endl;
+        cout << "+-----------------------------------------------+" << endl;
+        cout << "|  Bienvenido al sistema de la Univeridad umg   |" << endl;
+        cout << "+-----------------------------------------------+" << endl;
+        cout << "|            1. Agregar Registros               |" << endl;
+        cout << "|            2. Salida                          |" << endl;
+        cout << "+-----------------------------------------------+" << endl;
+        cout << "|            Ingrese su opcion [1/2]            |" << endl;
+        cout << "+-----------------------------------------------+" << endl;
+        cin >> opcion;
 
-    } while ((elec!=1) && (elec!=2));
-
-    if (elec==1)
-    {
-        cout << "\n" << endl;
-        cout << "           En este apartado entra el crud de alumnos" << endl;
-        cout << "\n" << endl;
-    }
-    else if (elec==2)
-    {
-        cout << "\n" << endl;
-        cout << "          En este apartado entra el crud de maestros" << endl;
-        cout << "\n" << endl;
-    }
+        switch (opcion) {
+            case 1:
+                system("cls");
+                cout << "\n"  << endl;
+                cout << "Opcion donde abrira el menu de registros" << endl;
+                cout << "\n"  << endl;
+                system("pause");
+                break;
+            case 2:
+                cout << "Saliendo del programa..." << endl;
+                continuar = 'n';
+                break;
+            default:
+                cout << "Opcion invalida. Por favor, ingrese una opcion valida." << endl;
+                break;
+        }
+    } while (continuar != 'n');
 
     return 0;
 }
